0be2a6a4d2f3e73c8d790df1ac02432ea404d2c8
Connor Unsworth <connor_unsworth@me.com>
2016-04-28 11:01:03 +0100

    v0.9

--- PSLogger.ps1
+++ PSLogger.ps1
@@ -20,6 +20,70 @@
 #>
 
 
+    
+  [System.Windows.Forms.Application]::EnableVisualStyles();
+  [void][reflection.assembly]::LoadWithPartialName("System.Windows.Forms")
+
+  $file = (get-item 'splash.jpg')
+  $img = [System.Drawing.Image]::Fromfile($file);
+ 
+  
+  $InitialFormWindowState = New-Object 'System.Windows.Forms.FormWindowState'
+  # Form Events
+  $Form_StateCorrection_Load =
+  {
+    #Correct the initial state of the form to prevent the .Net maximized form 
+    $form.WindowState = $InitialFormWindowState
+  }
+  
+  $Form_Cleanup_FormClosed =
+  {
+    #Remove all event handlers from the controls
+    try
+    {
+      $form.remove_Load($form_Load)
+      $form.remove_Load($Form_StateCorrection_Load)
+      $form.remove_FormClosed($Form_Cleanup_FormClosed)
+    }
+    catch [Exception]
+    { }
+  }
+
+
+  $form = new-object Windows.Forms.Form
+  $form.Width = $img.Size.Width;
+  $form.Height =  $img.Size.Height;
+  $pictureBox = new-object Windows.Forms.PictureBox
+  $pictureBox.Width =  $img.Size.Width;
+  $pictureBox.Height =  $img.Size.Height;
+
+  $pictureBox.Image = $img;
+  $form.controls.add($pictureBox)
+  $form.Add_Shown( { $form.Activate() } )
+
+
+  #
+  $form.ControlBox = $False
+  $form.Cursor = "AppStarting"
+  $form.FormBorderStyle = 'FixedToolWindow'
+  $form.Name = "NAME OF FORM"
+  $form.ShowIcon = $False
+  $form.ShowInTaskbar = $False
+  $form.StartPosition = 'CenterScreen'
+  $form.Text = "psLogger"
+  
+  #Save the initial state of the form
+  $InitialFormWindowState = $form.WindowState
+  #Init the OnLoad event to correct the initial state of the form
+  $form.add_Load($Form_StateCorrection_Load)
+  #Clean up the control events
+  $form.add_FormClosed($Form_Cleanup_FormClosed)
+  #Show the Form
+  $form.Show() | Out-Null
+  start-sleep -Seconds 2
+  $form.Close()
+
+
     [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
     $dialog = New-Object System.Windows.Forms.OpenFileDialog
     $dialog = New-Object System.Windows.Forms.OpenFileDialog
@@ -34,12 +98,6 @@
     $dialog.ShowDialog()
     $dialog.FileName
 
-    if($dialog.FileName -eq $null)
-    {exit}
-    else
-    {
-        Import-CliXML $dialog.FileName | out-gridview -Title ($dialog.FileName.Split("\")[-1])
-        pause
-    }
-
+    Import-CliXML $dialog.FileName | out-gridview -Title ($dialog.FileName.Split("\")[-1])
+    pause
 
